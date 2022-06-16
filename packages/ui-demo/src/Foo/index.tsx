f48f774c40eac1ef861567f4b600bfbc10d44e42
jasonxs45 <jasonxs45@126.com>
2022-06-16 18:33:51 +0800

    lerna publish test

--- packages/ui-demo/src/Foo/index.tsx
+++ packages/ui-demo/src/Foo/index.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import { Button } from 'antd-mobile-a';
 
-export default ({ title }: { title: string }) => <h1>{title}<Button>Click Me!</Button></h1>;
+export default ({ title }: { title: string }) => <h1>{title}<Button></Button></h1>;
